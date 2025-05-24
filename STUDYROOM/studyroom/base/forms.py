252322a4b136a5066b3de7a3199a3fd42077d029
Anik Dutta <anikdutta1997@gmail.com>
2025-05-24 10:26:07 +0530

    Minor fixes to the custom user model

--- STUDYROOM/studyroom/base/forms.py
+++ STUDYROOM/studyroom/base/forms.py
@@ -6,21 +6,15 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['name','username', 'email', 'password1', 'password2','bio', 'avatar']
 
-    def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password'})
+   
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
-
 
 class UserForm(ModelForm):
     class Meta:
--- STUDYROOM/studyroom/base/models.py
+++ STUDYROOM/studyroom/base/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     # Add any additional fields you want to include in the User model
     
-    name = models.CharField(max_length=200,unique=True,blank=True)
+    name = models.CharField(max_length=200, blank=True)
     username = models.CharField(max_length=200, unique=True, null=True, blank=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, default='avatar.svg')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 class Topic(models.Model):
     
     name = models.CharField(max_length=200, null=False,)
